fe3ec899cbff0c0c7511762759cb72ff234fc093
rshen <RogerShen1988@me.com>
2024-12-29 15:16:25 -0800

    Update ceo.tsx

--- svrt-project/app/homepages/ceo.tsx
+++ svrt-project/app/homepages/ceo.tsx
@@ -7,14 +7,13 @@ export default function Ceo() {
     return (
         <div>
             <div className={styles['centering']}>
-                <p className={styles['ceo-maintext']}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.</p>
+                <p className={styles['ceo-maintext']}>Partnering with SVRT lets us access innovative PropTech solutions, mentor startups, and stay ahead of industry trends. Together, we combine expertise to drive mutual growth and create value for the real estate industry.</p>
             </div>
             <div className={styles['circle-icon']}>
             <CircleIcon className={styles['circle']}/>
             </div>
             <div className={styles['first-name']}>
-                <p className={styles['bold-text']}>First Name</p>
+                <p className={styles['bold-text']}>Stephanie</p>
                 <p className={styles['ceo-description']}>, CEO of AlphaX RE Capital</p>
             </div>
         </div>
