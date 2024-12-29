215182257ccfa36cb3eab1648fb808eccbe935cd
rshen <RogerShen1988@me.com>
2024-12-29 15:10:38 -0800

    Update upcoming.tsx

--- svrt-project/app/homepages/upcoming.tsx
+++ svrt-project/app/homepages/upcoming.tsx
@@ -7,7 +7,7 @@ export default function Upcoming() {
     return (
         <div>
             <div className={styles['make-closer']}>
-                <p className={styles['upcoming-title']}>Upcoming</p>
+                <p className={styles['upcoming-title']}>Upcoming & Past</p>
                 <div className={styles['events-and-more']}>
                     <p className={styles['upcoming-title']}>Events</p>
                     <p className={styles['more-events']}>MORE EVENTS</p>
@@ -24,32 +24,32 @@ export default function Upcoming() {
                 <div className={styles['all-events']}>
                     <hr className={styles['my-line']} style={{marginTop: '-3rem'}}/>
                     <div className={styles['eventtitle-and-arrow']}>
-                        <p className={styles['end-of-year']}>End-of-Year Celebration with Leaders from PropTech</p>
+                        <p className={styles['end-of-year']}>From a VC's Eyes -- How Will I Evaluate a Startup</p>
                         <NorthEastRoundedIcon className={styles['myy-arrow']}/>
                     </div>
                     <div className={styles['small-text']}>
-                        <p>San Francisco, CA</p>
-                        <p>Dec 17, 2024</p>
+                        <p>Mountain View, CA</p>
+                        <p>Jan 11, 2025</p>
                     </div>
                     <hr className={styles['my-line']} style={{marginTop: '-3rem'}}/>
 
                     <div className={styles['eventtitle-and-arrow']}>
-                        <p className={styles['end-of-year']}>SVRT Demo Day</p>
+                        <p className={styles['end-of-year']}>Residential Real Estate Development -- A Full Pipeline Overview</p>
                         <NorthEastRoundedIcon className={styles['myy-arrow']}/>
                     </div>
                     <div className={styles['small-text']}>
-                        <p>San Francisco, CA</p>
-                        <p>Dec 17, 2024</p>
+                        <p>Mountain View, CA</p>
+                        <p>Jun 15, 2024</p>
                     </div>
                     <hr className={styles['my-line']} style={{marginTop: '-3rem'}}/>
 
                     <div className={styles['eventtitle-and-arrow']}>
-                        <p className={styles['end-of-year']}>Effective Investor Communication</p>
+                        <p className={styles['end-of-year']}>PRET 2024</p>
                         <NorthEastRoundedIcon className={styles['myy-arrow']}/>
                     </div>
                     <div className={styles['small-text']}>
-                        <p>San Francisco, CA</p>
-                        <p>Dec 17, 2024</p>
+                        <p>San Jose, CA</p>
+                        <p>May 11, 2024</p>
                     </div>
                     <hr className={styles['my-line']} style={{marginTop: '-3rem'}}/>
                 </div>
