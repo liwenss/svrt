6755cdb5f1f3c88a3ceea83048c3e84fad24ed28
rshen <RogerShen1988@me.com>
2025-01-11 16:10:41 -0800

    Update page.tsx

--- svrt-project/app/partners/page.tsx
+++ svrt-project/app/partners/page.tsx
@@ -9,9 +9,11 @@ export default function Page() {
   return (
     <div className={styles['padding']}>
       <p className={styles['title']}>We partner with</p>
-      <p className={styles['maintext']}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-        dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua.</p>
+      <p className={styles['maintext']}>SVRT proudly collaborates with industry-leading companies across the entire real estate value chain, 
+        including acquisition, financing, development and construction, asset management, and disposition. These strategic partnerships enable 
+        us to offer unparalleled resources and expertise to our portfolio companies. Whether you're seeking collaborations, contracts, or direct 
+        financing, our network provides the support and connections necessary to drive innovation and growth in the real estate sector. 
+        Reach out to us to explore how we can help your business thrive.</p>
       <div className={styles['image-and-content']}>
         <Image
           src='/logo9.png'
