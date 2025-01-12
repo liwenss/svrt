3165c57a04f12249df95732b65d84a42df61ae36
rshen <RogerShen1988@me.com>
2025-01-11 16:57:45 -0800

    Update page.tsx removed extra rows

--- svrt-project/app/investments/page.tsx
+++ svrt-project/app/investments/page.tsx
@@ -105,22 +105,6 @@ export default function Page() {
       </div>
       <hr className={styles['my-line']}/>
 
-      <div className={styles['image-and-content']}>
-        <Image
-          src='/logo9.png'
-          alt="Description of the image"
-          width={254}
-          height={1080}
-        />
-        <div className={styles['right-half']}>
-          <div className={styles['company-and-tag']}>
-            <p className={styles['another-maintext']}>Company Name</p>
-            <Button className={styles['button-text']}>Tag-category</Button>
-          </div>
-          <p className={styles['subtext']}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua.</p>
-        </div>
-      </div>
       <div className={styles['some-spacing']}></div>
       <Bottombar/>
     </div>
